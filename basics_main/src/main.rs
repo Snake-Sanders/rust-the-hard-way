14bfcbb85147581eb09abb4aa00d0b9784e5f5aa
Snake Sanders <elmarian22@gmail.com>
2023-07-09 17:29:16 +0200

    autoformat

--- basics_main/src/main.rs
+++ basics_main/src/main.rs
@@ -3,6 +3,8 @@ use std::io;
 fn main() {
     let mut user_name = String::new();
     println!("Enter your name:");
-    io::stdin().read_line(&mut user_name).expect("Failed to read user_name");
+    io::stdin()
+        .read_line(&mut user_name)
+        .expect("Failed to read user_name");
     println!("Hello, {}!", user_name.trim());
 }
